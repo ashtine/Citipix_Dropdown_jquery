4d4b8f8835f71216ab132bad5158a5ff5d471a1f
ashtine <ashtinet@yahoo.com>
2017-03-11 16:21:34 -0500

    successfully working for each city
    got all cities to load in with appropriate pictures of the city. Code
    can still be refactored to be simpler.

--- js/jquery.js
+++ js/jquery.js
@@ -3,11 +3,9 @@ $(document).ready(function(){
 $('select').append('<option>NYC</option>');
 $('select').append('<option>SF</option>');
 $('select').append('<option>SYD</option>');
+$('select').append('<option>Austin</option>');
+$('select').append('<option>LA</option>');
 
-                // <option>NYC</option>
-                // <option>SYD</option>
-                // <option>Austin</option>
-                // <option>LA</option>
     $('#city-type').change(function(event){
         event.preventDefault();
         var userInput = $('#city-type').val();
@@ -19,6 +17,10 @@ $('select').append('<option>SYD</option>');
             $('body').attr("class", "sf");
         } else if (userInput == "syd"){
             $('body').attr("class", "sydney");
+        } else if (userInput == "austin"){
+            $('body').attr("class", "austin");
+        } else if (userInput == "la"){
+            $('body').attr("class", "la");
         }
      }); 
 
