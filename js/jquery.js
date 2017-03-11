7a987d6c2dfad3dd66b6d22d4ce80f64c9932e73
ashtine <ashtinet@yahoo.com>
2017-03-11 16:16:07 -0500

    added .append
    removed the html from index and added it into the jquery

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-    
+
+$('select').append('<option>NYC</option>');
+$('select').append('<option>SF</option>');
+$('select').append('<option>SYD</option>');
+
+                // <option>NYC</option>
+                // <option>SYD</option>
+                // <option>Austin</option>
+                // <option>LA</option>
+    $('#city-type').change(function(event){
+        event.preventDefault();
+        var userInput = $('#city-type').val();
+        userInput = userInput.toLowerCase();
+        console.log("this what is inside the input field:", userInput);
+        if (userInput == "nyc") {
+            $('body').attr("class", "nyc");
+        } else if (userInput == "sf"){
+            $('body').attr("class", "sf");
+        } else if (userInput == "syd"){
+            $('body').attr("class", "sydney");
+        }
+     }); 
+
 });
